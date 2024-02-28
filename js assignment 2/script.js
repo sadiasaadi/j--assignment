0216d650dd9dcccd487d74c3048b2280136bec4d
sadiasaadi <159929183+sadiasaadi@users.noreply.github.com>
2024-02-28 18:10:36 +0500

    Add files via upload

---
+++ js assignment 2/script.js
@@ -0,0 +1,164 @@
+// chapter 4 variables names : legal & illegal
+
+// q1
+
+var varibaleOme = "two";
+var varibaleTwo = "2";
+var varibaleThree = "3";
+
+
+// q2
+
+// legal variable
+
+var varibaleOne = " jjy";
+var varibalecvce = "gfhjg";
+var varibalevhe = "gfbmg";
+var varibalevve = "gjhfg";
+var varibalevge = "gffse";
+
+
+// illegal variable
+
+// var 1varibal2 = "gffg";
+// var 2varibaleOne = "gffg";
+// var 3varibaleOne = "gffg";
+// var 4varibaleOne = "gffg";
+// var 5varibaleOne = "gffg";
+
+// chapter 5
+
+
+
+// chapter 6 math expression 
+
+// q1
+
+var x = 1
+var num = x;
+var total = (x+num);
+alert(total);
+
+// q2
+
+var x = 100;
+var number = --x;
+console.log(number);
+
+// q3
+
+var x = 50;
+var y = x++;
+console.log(y);
+
+// q4
+
+var y  = 50;
+var z = --y;
+console.log(z);
+
+// q5
+
+var num = 70;
+var newNum= --num;
+console.log(newNum);
+
+
+
+// q6
+
+var number = 34;
+var inc = ++number;
+alert(inc);
+
+
+// chapter 7 math expression
+
+// q1
+
+var calculatedNumber = 2 +(2*6);
+console.log(calculatedNumber);
+
+// q2
+
+var calculatedNumber = (2+2)*6;
+console.log(calculatedNumber);
+
+// q3
+
+var calculatedNumber = (2+2)*(4+2);
+console.log(calculatedNumber);
+
+// q4
+var calculatedNumber = ((2+2)* 4)+2 ;
+console.log(calculatedNumber);
+
+
+
+//  chapter 8 concatenating text strings 
+
+// q1
+
+var number = "2" + "2";
+console.log(number);
+
+// q2
+
+var message = "Hello" + "Dolly";
+alert(message);
+
+// q3
+
+var message = ("33" + 3);
+alert(message);
+
+// q4
+
+var partOne = "pakistan";
+var partTwo = "zindabad";
+var final = (partOne + partTwo);
+alert(final);
+
+
+// chapter 9 prmpts
+
+// q1
+
+var firstName = prompt("enter first name"); 
+console.log("firstName" + " " + firstName);
+
+
+// q2
+
+var country = prompt("country" , "china");
+console.log( country);
+
+// q3
+
+var yourName = prompt ("enter your name");
+
+
+// q4
+
+var userInput = prompt("userInput" , "omer");
+console.log(userInput);
+
+
+
+// q5
+
+var firstVariable = ("enter your message");
+var secondVariable = ("default message" , "default");
+var userResponse = prompt(firstVariable , secondVariable);
+console.log(userResponse)
+
+
+// q6
+
+var firstVariable = ("enter your message");
+var secondVariable = ("default message" , "default");
+var finalInput = prompt(firstVariable, secondVariable);
+alert(finalInput);
+
+
+// the end
